b8eaa47fb412228f7c81c406bf1a84af214674c6
Greg Pfeil <greg@technomadic.org>
2023-08-30 11:28:00 -0600

    fixup! Modularize the project.nix to make it readable

--- .config/project.nix
+++ .config/project.nix
@@ -5,19 +5,23 @@
     license = "MIT"; # Induced by this being basically a fork of Home Manager
     ## Packages to install in the devShells that reference projectConfiguration.
     packages = [
-      pkgs.nil
+      ## language servers
+      pkgs.nil # Nix
       pkgs.nodePackages.bash-language-server
     ];
   };
 
   imports = [
+    ## tooling
     ./direnv.nix
-    ./editorconfig.nix
-    ./emacs.nix
-    ./garnix.nix
     ./git.nix
+    ./shellcheck.nix
+    ## services
+    ./garnix.nix
     ./github.nix
     ./renovate.nix
-    ./shellcheck.nix
+    ## editors
+    ./editorconfig.nix
+    ./emacs.nix
   ];
 }
