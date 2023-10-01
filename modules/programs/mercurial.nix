3eda2f75bc1ac48b436d4667f24187b63d0f508a
Greg Pfeil <greg@technomadic.org>
2023-10-01 00:28:57 -0600

    Include mercurial package when enabled

--- modules/programs/mercurial.nix
+++ modules/programs/mercurial.nix
@@ -68,7 +68,7 @@ in {
 
   config = mkIf cfg.enable (mkMerge [
     {
-      # home.packages = [cfg.package];
+      project.packages = [cfg.package];
 
       project.file.".hg/hgrc" = {
         persistence = "store";
