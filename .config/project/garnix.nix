4280965968c2417612219064b878c8297c6de6f1
Greg Pfeil <greg@technomadic.org>
2023-10-18 23:31:53 -0600

    Fix the up-to-date check
    (cherry picked from commit de7b23cfb706442ddc016a1c3223c62674bd7590)

--- .config/project/garnix.nix
+++ .config/project/garnix.nix
@@ -2,8 +2,12 @@
   services.garnix = {
     enable = true;
     builds = {
-      # TODO: Remove once garnix-io/garnix#285 is fixed.
-      exclude = ["homeConfigurations.x86_64-darwin-${config.project.name}-example"];
+      exclude = [
+        ## TODO: This check requires Internet access.
+        "checks.*.project-manager-files"
+        # TODO: Remove once garnix-io/garnix#285 is fixed.
+        "homeConfigurations.x86_64-darwin-${config.project.name}-example"
+      ];
       include = ["*.*" "*.*.*"];
     };
   };
