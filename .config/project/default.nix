8c926c0c1c798bf84933be434916688e4441602b
Greg Pfeil <greg@technomadic.org>
2023-11-18 10:10:05 -0700

    Remove support for old ShellCheck module

--- .config/project/default.nix
+++ .config/project/default.nix
@@ -34,18 +34,7 @@
   ## formatting
   editorconfig.enable = true;
   programs = {
-    shellcheck = {
-      enable = true;
-      ## TODO: Move this up to flaky.
-      directives.disable = [
-        # Unicode quotes are good, and ShellCheck gets this wrong a lot.
-        "SC1111"
-        "SC1112"
-        # `shfmt` likes to replace `"\$foo"` with '$foo', which then triggers this
-        # complaint, so let shfmt do what it wants.
-        "SC2016"
-      ];
-    };
+    shellcheck.enable = true;
     treefmt = {
       enable = true;
       ## Shell linter
