4ca5c0c75b72ca91cc6b5166879c6325b4b38d75
Greg Pfeil <greg@technomadic.org>
2023-11-27 15:09:13 -0700

    Enrich the projectConfigurations schema

--- nix/schemas.nix
+++ nix/schemas.nix
@@ -1,14 +1,75 @@
-{flake-schemas}: {
+{flake-schemas}: let
+  ## Stolen from DeterminateSystems/flake-schemas, which should export it in its
+  ## lib.
+  checkDerivation = drv: drv.type or null == "derivation" && drv ? drvPath;
+in {
   projectConfigurations = {
     version = 1;
     doc = ''
       The `projectConfigurations` flake output defines project configurations.
     '';
     inventory = output:
-      flake-schemas.lib.mkChildren (builtins.mapAttrs (system: project: {
+      flake-schemas.lib.mkChildren (builtins.mapAttrs (system: project: let
           forSystems = [system];
+        in {
+          inherit forSystems;
           what = "Project Manager configuration for this flake’s project";
-          derivation = project.activationPackage;
+          children = {
+            activationPackage = {
+              inherit forSystems;
+              shortDescription = project.activationPackage.meta.description;
+              derivation = project.activationPackage;
+              evalChecks.isDerivation = checkDerivation project.activationPackage;
+              what = "package";
+              isFlakeCheck = false;
+            };
+            checks = flake-schemas.lib.mkChildren (builtins.mapAttrs (checkName: check: {
+                inherit forSystems;
+                shortDescription = check.meta.description or "";
+                derivation = check;
+                evalChecks.isDerivation = checkDerivation check;
+                what = "CI test";
+                isFlakeCheck = true;
+              })
+              project.checks);
+            devShells = flake-schemas.lib.mkChildren (builtins.mapAttrs (shellName: shell: {
+                inherit forSystems;
+                shortDescription = shell.meta.description;
+                derivation = shell;
+                evalChecks.isDerivation = checkDerivation shell;
+                what = "development environment";
+                isFlakeCheck = false;
+              })
+              project.devShells);
+            formatter = {
+              inherit forSystems;
+              shortDescription = project.formatter.meta.description or "";
+              derivation = project.formatter;
+              evalChecks.isDerivation = checkDerivation project.formatter;
+              what = "Nix code formatter";
+              isFlakeCheck = false;
+            };
+            filterRepositoryPersistedExcept = {
+              shortDescription = project.option.filterRepositoryPersistedExcept.description;
+              what = "source filter";
+              isFlakeCheck = false;
+            };
+            filterRepositoryPersisted = {
+              shortDescription = project.option.filterRepositoryPersisted.description;
+              what = "source filter";
+              isFlakeCheck = false;
+            };
+            cleanRepositoryPersistedExcept = {
+              shortDescription = project.option.cleanRepositoryPersistedExcept.description;
+              what = "source filter";
+              isFlakeCheck = false;
+            };
+            cleanRepositoryPersisted = {
+              shortDescription = project.option.cleanRepositoryPersisted.description;
+              what = "source filter";
+              isFlakeCheck = false;
+            };
+          };
         })
         output);
   };
