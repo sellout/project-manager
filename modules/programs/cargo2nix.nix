5b69d101c3278e82132d14da08b14839e8473353
Greg Pfeil <greg@technomadic.org>
2025-08-11 15:02:32 -0600

    Generate Cargo.nix to Nix store

--- modules/programs/cargo2nix.nix
+++ modules/programs/cargo2nix.nix
@@ -36,28 +36,6 @@ in {
       '';
     };
 
-    file = lib.mkOption {
-      type =
-        lib.types.nullOr (lib.types.pathWith {});
-      default = null;
-      example = "./nix/Cargo.nix";
-      description = ''
-        Output to filepath (the default is ./Cargo.nix).
-      '';
-    };
-
-    overwrite = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      example = true;
-      description = ''
-        Overwrite existing output filepath without prompting.
-
-        __TODO__: If the persistence of this file is `repository`, then this
-                 _must_ be true, as the file will always exist.
-      '';
-    };
-
     locked = lib.mkOption {
       type = lib.types.bool;
       default = false;
@@ -66,41 +44,33 @@ in {
         Don't attempt to update the lockfile.
       '';
     };
+
+    packageFun = lib.mkOption {
+      type = lib.types.package;
+      readOnly = true;
+      description = ''
+        A package containing the Cargo.nix file. This is used to reference the
+        generated file in the flake.
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
     ## TODO: Use real persistence settings for this file, but for now, just
     ##       treat it as `worktree`.
-    programs = let
-      cargoNix = if cfg.file != null then cfg.file else "./Cargo.nix";
-    in {
-      git.ignores = [cargoNix];
-      treefmt.settings.global.excludes = [cargoNix];
-    };
+    programs.cargo2nix.packageFun = let
+      options =
+        lib.optionals (cfg.completions != null)
+        ["--completions" (lib.escapeShellArg cfg.completions)]
+        ++ ["--file" "$out"]
+        ++ lib.optional cfg.locked "--locked"
+        ++ lib.optional (cfg.workspaceDirectory != null)
+        (lib.escapeShellArg cfg.workspaceDirectory);
+    in
+      pkgs.runCommand "Cargo.nix" {} ''
+        ${lib.getExe cfg.package} ${lib.concatStringsSep " " options}
+      '';
 
     project.devPackages = [cfg.package];
-
-    ## TODO: Rather than generating a file directly into the tree, generate to
-    ##       the Nix store and symlink or copy via `project.file`.
-    project.activation.cargo2nix =
-      ## This ensures that any Cargo.lock file produced by cargo will be in
-      ## place when cargo2nix wants to generate its Nix files.
-      lib.pm.dag.entryAfter ["writeBoundary" "cargo-generate-lockfile"] (let
-        options =
-          lib.optionals (cfg.completions != null)
-          ["--completions" (lib.escapeShellArg cfg.completions)]
-          ++ lib.optionals (cfg.file != null)
-          ["--file" (lib.escapeShellArg cfg.file)]
-          ++ lib.optional cfg.overwrite "--overwrite"
-          ++ lib.optional cfg.locked "--locked"
-          ++ lib.optional (cfg.file == false) "--stdout"
-          ++ lib.optional (cfg.workspaceDirectory != null)
-          (lib.escapeShellArg cfg.workspaceDirectory);
-      in ''
-        ${if cfg.file != null
-          then "mkdir -p $(dirname ${lib.escapeShellArg cfg.file})"
-          else ""}
-        ${lib.getExe cfg.package} ${lib.concatStringsSep " " options}
-      '');
   };
 }
